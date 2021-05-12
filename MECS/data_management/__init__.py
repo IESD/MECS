368df5ab6ba5dfe5790b7c9ca995b26101572639
Graeme Stuart <ggstuart@gmail.com>
2021-05-12 11:44:11 +0100

    mecs-aggregate script to be scheduled via cron.
    Incomplete as it doesn't manage the data files.
    
    Code moved from aggregate_data module into package __init__ module.

--- MECS/data_management/__init__.py
+++ MECS/data_management/__init__.py
@@ -1,6 +1,19 @@
+"""
+Data management
+
+These are the primary functions available for the data management system
+
+generate: This is the high level source of data, a long-running process that generates a file on disk every minute.
+
+aggregate: This is for preparing data for upload, it should be scheduled to run every hour, it merges the available files into one and saves it in a folder ready for uploading.
+
+"""
+import glob
 import json
 import os.path
 import logging
+
+import pandas as pd
 
 from ..config import config
 from .write_output import aggregated_minutely_readings
@@ -8,7 +21,9 @@ from .write_output import aggregated_minutely_readings
 conf = config()
 log = logging.getLogger(__name__)
 
-OUTPUT_FOLDER = os.path.join(os.path.expanduser(conf['MECS']["root_folder"]), conf['MECS']["output_folder"])
+ROOT = os.path.expanduser(conf['MECS']["root_folder"])
+OUTPUT_FOLDER = os.path.join(ROOT, conf['MECS']["output_folder"])
+AGGREGATED_FOLDER = os.path.join(ROOT, conf['MECS']["aggregated_folder"])
 
 def generate():
     try:
@@ -24,3 +39,39 @@ def generate():
         log.debug(f"writing {path}")
         with open(path, "x") as f:
             json.dump(data, f)
+
+def aggregate():
+    try:
+        os.makedirs(AGGREGATED_FOLDER)
+        log.info(f"created folder: {AGGREGATED_FOLDER}")
+    except FileExistsError:
+        pass
+
+    # Grab all the filenames
+    files = sorted(glob.glob(os.path.join(OUTPUT_FOLDER, "*.json")))
+
+    if not len(files):
+        log.warning(f"No *.json files found in {OUTPUT_FOLDER}")
+        return
+
+    # read them into an array
+    log.info(f"Aggregating {len(files)} files from {OUTPUT_FOLDER} into {AGGREGATED_FOLDER}")
+    result = []
+    for filename in files:
+        with open(filename, 'r') as f:
+            data = json.load(f)
+        result.append(data)
+        # TODO: move file into temporary folder ready for deletion/archive?
+
+    # convert the array into a pandas dataframe
+    df = pd.DataFrame(result)
+
+    # add the datetime index and sort
+    df.set_index("dt", inplace=True)
+    df.sort_index(inplace=True)
+
+    # Write the aggregated data
+    write_path = os.path.join(AGGREGATED_FOLDER, f"{df.index[0]}-{df.index[-1]}.json")
+    log.debug(f"writing {path}")
+    with open(write_file, "x") as f:
+        json.dump(df.to_json(), f)
--- setup.py
+++ setup.py
@@ -13,5 +13,6 @@ setup(
     [console_scripts]
     mecs-init = MECS.initialisation:main
     mecs-generate = MECS.data_management:generate
+    mecs-aggregate = MECS.data_management:aggregate
     """
 )
