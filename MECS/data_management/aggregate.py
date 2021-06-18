4e0462862b6bcbfe6a700e21c770df8ed52416fb
Graeme Stuart <ggstuart@gmail.com>
2021-06-18 14:03:58 +0100

    When aggregating data, create an archive for any folders (%Y%m%d) **before** the current date.

--- MECS/data_management/aggregate.py
+++ MECS/data_management/aggregate.py
@@ -4,26 +4,49 @@ it should be scheduled to run every hour,
 it merges the available files into one file per day
 and saves it in a folder ready for uploading.
 """
+from datetime import date
 import glob
 import json
 import os.path
+import shutil
+import tarfile
 import logging
 
 import pandas as pd
 
 log = logging.getLogger(__name__)
 
+def make_archive_file(output_filename, source_dir):
+    """converts a folder full of minutely json files into a single, compressed archive file"""
+    with tarfile.open(output_filename, "w:gz") as tar:
+        tar.add(source_dir, arcname=os.path.basename(source_dir))
+
 def aggregate(source_folder, destination_folder):
+    """
+    Converts minutely files for a day into a single file
+    Creates an aggregated file in the destination directory for each folder in the source directory
+    """
+    today = date.today().strftime("%Y%m%d")
     os.makedirs(destination_folder, exist_ok=True)
-    folders = sorted(glob.glob(os.path.join(source_folder, "*")))
+    folders = sorted(next(os.walk(source_folder))[1])
     for folder in folders:
-        df = aggregate_folder(folder)
-        day = df.index[0].strftime("%Y%m%d")
-        path = os.path.join(destination_folder, f"{day}.json")
-        log.info(f"writing to {path}")
-        df.to_json(path, orient="split", date_format="iso")
+        source_path = os.path.join(source_folder, folder)
+        aggregated_path = os.path.join(destination_folder, f"{folder}.json")
+        df = aggregate_folder(source_path)
+        log.info(f"writing to {aggregated_path}")
+        df.to_json(aggregated_path, orient="split", date_format="iso")
+
+        # archiving when the data is from before today
+        if(folder < today):
+            archive_path = os.path.join(source_folder, f"{folder}.tar.gz")
+            log.info(f"archiving {source_path} to {archive_path}")
+            make_archive_file(archive_path, source_path)
+            log.info(f"deleting original {source_path}")
+            shutil.rmtree(source_path)
+
 
 def aggregate_folder(folder):
+    """This does the aggregation job, merging the files into one dataframe"""
     files = sorted(glob.glob(os.path.join(folder, "*.json")))
     if not len(files):
         log.warning(f"No *.json files found in {folder}")
