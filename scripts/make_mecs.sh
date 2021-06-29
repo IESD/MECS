c5b821a117226f51473b7219d4f633ba84e6a9bd
Richard Snape <rsnape@dmu.ac.uk>
2021-06-29 17:32:56 +0100

    Remove relative paths and install pip

--- scripts/make_mecs.sh
+++ scripts/make_mecs.sh
@@ -12,14 +12,10 @@ fi
 
 # 2. apt-get install stuff
 apt install git
+apt install python3-pip
 
-# 2a. Pull the git repo
+# 2a. Clone the git repo
 git clone https://github.com/IESD/MECS.git
-
-# 3. copy scripts to /usr/local/bin
-# Copy the waveshare setup script to /usr/local/bin
-cp ./waveshare_gpio_init.sh /usr/local/bin/
-chmod 777 /usr/local/bin/waveshare_gpio_init.sh
 
 # 4. Prepare python
 # 4a. pip install some annoying dependencies
@@ -27,10 +23,10 @@ pip3 install numpy
 pip3 install pandas
 
 # 4b. Install MECS
-python3 setup.py install
+python3 MECS/setup.py install
 
 # 5. register service definitions with systemd
-cp ../services/* /etc/system.d/system
+cp MECS/services/* /etc/system.d/system
 
 # 6. enable services
 systemctl enable mecs-generate.service
@@ -38,7 +34,12 @@ systemctl enable waveshare-gpio.service
 
 # 7. install crontabs
 # specify pi user for the pi user cron
-sudo -u pi crontab ../cron/pi.cron
-crontab ../cron/root.cron
+sudo -u pi crontab MECS/cron/pi.cron
+crontab MECS/cron/root.cron
 
-# 8. reboot
+# 8. copy scripts to /usr/local/bin
+# Copy the waveshare setup script to /usr/local/bin
+cp MECS/waveshare_gpio_init.sh /usr/local/bin/
+chmod 777 /usr/local/bin/waveshare_gpio_init.sh
+
+# 9. reboot
