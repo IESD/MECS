b0d64af196f6cd59804cc978a24732e85334f4de
Graeme Stuart <ggstuart@gmail.com>
2021-09-03 13:12:14 +0100

    board now requires two config objects on initialisation

--- MECS/data_acquisition/board.py
+++ MECS/data_acquisition/board.py
@@ -16,26 +16,24 @@ from .temperature import TemperatureThing
 log = logging.getLogger(__name__)
 
 class MECSBoard:
-    def __init__(self, conf):
+    def __init__(self, conf, calibration):
         self.adc = ADCThing(conf["ADCPi"]) # config determines bit_rate and impedance
         self.ina3221 = INA3221Thing()
         self.sds011 = SDS011Thing()
         self.w1_therm = TemperatureThing()
-        registerables = {
+        self.registerables = {
             "adc": self.adc,
             "INA3221": self.ina3221,
             "SDS011": self.sds011,
             "W1THERM": self.w1_therm
         }
-        calibration_file_path = conf['data-acquisition']['calibration_file']
-        self.calibration_config = ConfigParser()
-        self.calibration_config.read(calibration_file_path)
+        self.calibration_config = calibration
         for key in self.calibration_config:
             if key == "DEFAULT": continue
             cal = self.calibration_config[key]
             protocol = cal.get("protocol", fallback="unknown")
             try:
-                registerables[protocol].register(key, cal)
+                self.registerables[protocol].register(key, cal)
             except KeyError as ex:
                 log.warning(f"ignoring [{key}] unsupported protocol: {protocol}")
             except MECSConfigError as exc:
