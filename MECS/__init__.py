9fc1674fe05f0add84200938347157a5de1552f4
Graeme Stuart <ggstuart@gmail.com>
2022-04-06 15:40:08 +0100

    consolidate mecs-update

--- MECS/__init__.py
+++ MECS/__init__.py
@@ -7,32 +7,9 @@ It includes packages for:
 """
 
 import pkg_resources
-import subprocess
-import os
-
-import logging
-
-log = logging.getLogger(__name__)
 
 __version__ = pkg_resources.get_distribution('MECS').version
 
 class MECSError(Exception): pass
 class MECSConfigError(MECSError): pass
 class MECSHardwareError(MECSError): pass
-
-
-def update_mecs(path, branch, full=False):
-    """This function will:
-        ping google to restart comms
-        pull the latest commits from github
-        run setup.py to reinstall the software
-    """
-    os.chdir(path)
-    ping_stat = subprocess.run(["ping", "8.8.8.8","-w","5"], capture_output=True)
-    if ping_stat.returncode != 0:
-        log.warning('Ping did not exit cleanly, outside network connectivity lost?')
-        log.warning('StdOut:' + ping_stat.stdout)
-        log.warning('StdErr:' + ping_stat.stderr)
-    subprocess.run(["git", "pull", "origin", branch])
-    subprocess.run(["python3", "setup.py", "install" if full else "develop"])
-
--- MECS/cli/update.py
+++ MECS/cli/update.py
@@ -1,13 +1,13 @@
 
 import logging
 import os
+import subprocess
 from configparser import NoOptionError
 
 from .. import __version__, update_mecs
 from ..config import conf, args
 
 log = logging.getLogger(__name__)
-
 
 def update():
     log.debug(f"MECS v{__version__} updating installation")
@@ -21,3 +21,21 @@ def update():
         log.warning("required for code update")
         exit(0)
     update_mecs(GIT_PATH, GIT_BRANCH, full=FULL_INSTALL)
+
+
+
+def update_mecs(path, branch, full=False):
+    """This function will:
+        ping google to restart comms
+        pull the latest commits from github
+        run setup.py to reinstall the software
+    """
+    os.chdir(path)
+    ping_stat = subprocess.run(["ping", "8.8.8.8","-w","5"], capture_output=True)
+    if ping_stat.returncode != 0:
+        log.warning('Ping did not exit cleanly, outside network connectivity lost?')
+        log.warning('StdOut:' + ping_stat.stdout)
+        log.warning('StdErr:' + ping_stat.stderr)
+    subprocess.run(["git", "pull", "origin", branch])
+    subprocess.run(["python3", "setup.py", "install" if full else "develop"])
+
